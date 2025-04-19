80f260bce6704a42fdcb7a986028678b5042ee18
Huang1810 <ahhamed1810@gmail.com>
2025-04-19 22:32:30 +0100

    commit

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -2,90 +2,11 @@
 #include <stdlib.h>
 
 /**
- * swap_values - Swaps the values of two nodes
- * @a: First node
- * @b: Second node
- */
-void swap_values(heap_t *a, heap_t *b)
-{
-	/* Swapping values between nodes */
-	int temp = a->n;
-	a->n = b->n;
-	b->n = temp;
-}
-
-/**
- * heapify_up - Maintains the max-heap property by bubbling up
- * @node: Pointer to the node to heapify
- * Return: Pointer to the final position of the node
- */
-heap_t *heapify_up(heap_t *node)
-{
-	while (node->parent && node->n > node->parent->n)
-	{
-		swap_values(node, node->parent);
-		node = node->parent;
-	}
-	return (node);
-}
-
-/**
- * get_size - Measures the size of a binary tree
- * @root: Pointer to the root node
- * Return: Number of nodes
- */
-size_t get_size(const heap_t *root)
-{
-	if (!root)
-		return (0);
-	return (1 + get_size(root->left) + get_size(root->right));
-}
-
-/**
- * get_node_at_index - Gets the node at a given index (level-order)
- * @root: Pointer to the root node
- * @index: Index of the node to find
- * Return: Pointer to the node, or NULL if not found
- */
-heap_t *get_node_at_index(heap_t *root, size_t index)
-{
-	if (!root || index == 0)
-		return (root);
-
-	size_t count = 0;
-	heap_t **queue = malloc(sizeof(heap_t *) * 1024);
-	heap_t *node = NULL;
-
-	if (!queue)
-		return (NULL);
-
-	queue[count++] = root;
-	size_t i = 0;
-
-	while (i < count)
-	{
-		node = queue[i];
-		if (i == index)
-			break;
-
-		if (node->left)
-			queue[count++] = node->left;
-		if (node->right)
-			queue[count++] = node->right;
-
-		i++;
-	}
-
-	heap_t *result = (i == index) ? node : NULL;
-	free(queue);
-	return (result);
-}
-
-/**
  * heap_insert - Inserts a value into a Max Binary Heap
- * @root: Double pointer to the root node of the Heap
- * @value: Value to store in the node to be inserted
- * Return: Pointer to the inserted node, or NULL on failure
+ * @root: A double pointer to the root node of the Heap
+ * @value: The value to store in the node to be inserted
+ *
+ * Return: A pointer to the created node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
@@ -96,6 +17,7 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (NULL);
 
 	new_node = binary_tree_node(NULL, value);
+
 	if (!new_node)
 		return (NULL);
 
@@ -105,13 +27,8 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (new_node);
 	}
 
-	size = get_size(*root);
+	size = binary_tree_size(*root);
 	parent = get_node_at_index(*root, (size - 1) / 2);
-	if (!parent)
-	{
-		free(new_node);
-		return (NULL);
-	}
 
 	new_node->parent = parent;
 	if (!parent->left)
@@ -119,5 +36,76 @@ heap_t *heap_insert(heap_t **root, int value)
 	else
 		parent->right = new_node;
 
-	return (heapify_up(new_node));
+	heapify_up(new_node);
+	return (new_node);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
+ *
+ * Return: The size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * get_node_at_index - Gets the node at a given index (level-order)
+ * @root: Pointer to the root node
+ * @index: Index of the node to retrieve
+ *
+ * Return: Pointer to the node at the index, or NULL if not found
+ */
+heap_t *get_node_at_index(heap_t *root, size_t index)
+{
+	heap_t *queue[1024];
+	size_t front = 0, rear = 0, count = 0;
+
+	if (!root)
+		return (NULL);
+
+	queue[rear++] = root;
+	count++;
+
+	size_t i = 0;
+
+	while (i < count)
+	{
+		heap_t *current = queue[i];
+
+		if (i == index)
+			return (current);
+
+		if (current->left)
+			queue[count++] = current->left;
+		if (current->right)
+			queue[count++] = current->right;
+
+		i++;
+	}
+
+	return (NULL);
+}
+
+/**
+ * heapify_up - Maintains the heap property after insertion
+ * @node: Pointer to the newly inserted node
+ */
+void heapify_up(heap_t *node)
+{
+	int temp;
+
+	while (node->parent && node->n > node->parent->n)
+	{
+		temp = node->n;
+		node->n = node->parent->n;
+		node->parent->n = temp;
+
+		node = node->parent;
+	}
 }
