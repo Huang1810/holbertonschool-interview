2c1e43035f8d5f1f612508bd2c9553ea46d91e97
Huang1810 <ahhamed1810@gmail.com>
2025-07-12 22:54:37 +0100

    commit

--- linear_skip/search.h
+++ linear_skip/search.h
@@ -1,7 +1,8 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
-#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
@@ -19,8 +20,30 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
+/* Function Prototypes */
+
+/**
+ * create_skiplist - Creates a skip list from an array
+ * @array: Pointer to array of integers
+ * @size: Number of elements in the array
+ *
+ * Return: Pointer to the head of the new skip list
+ */
+skiplist_t *create_skiplist(int *array, size_t size);
+
+/**
+ * free_skiplist - Frees the memory used by a skip list
+ * @list: Pointer to the head of the skip list
+ */
+void free_skiplist(skiplist_t *list);
+
+/**
+ * linear_skip - Searches for a value in a sorted skip list
+ * @list: Pointer to the head of the skip list
+ * @value: Value to search for
+ *
+ * Return: Pointer to node where value is located or NULL
+ */
 skiplist_t *linear_skip(skiplist_t *list, int value);
-skiplist_t *create_skiplist(int *array, size_t size);  /* <- add this */
-void free_skiplist(skiplist_t *list);                  /* <- and this */
 
 #endif /* SEARCH_H */
