b859f02f8dd7c2faf1f5b88f7a74bace257a5213
Huang1810 <ahhamed1810@gmail.com>
2025-08-31 00:59:38 +0100

    commit

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -50,46 +50,13 @@ void errors(void)
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: argument vector
- * Return: 0 on success
+ * print_result - prints the result array
+ * @res: array of integers
+ * @len_res: length of result
  */
-int main(int argc, char *argv[])
+void print_result(int *res, int len_res)
 {
-	char *num1, *num2;
-	int len1, len2, len_res, i, j, carry, n1, n2, *res;
 	int start = 0;
-
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-		errors();
-
-	num1 = argv[1];
-	num2 = argv[2];
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
-	len_res = len1 + len2;
-	res = malloc(sizeof(int) * (len_res));
-	if (!res)
-		return (1);
-
-	for (i = 0; i < len_res; i++)
-		res[i] = 0;
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		n1 = num1[i] - '0';
-		carry = 0;
-
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			n2 = num2[j] - '0';
-			carry += res[i + j + 1] + (n1 * n2);
-			res[i + j + 1] = carry % 10;
-			carry /= 10;
-		}
-		res[i + j + 1] += carry;
-	}
 
 	while (start < len_res && res[start] == 0)
 		start++;
@@ -102,6 +69,56 @@ int main(int argc, char *argv[])
 			_putchar(res[start] + '0');
 	}
 	_putchar('\n');
+}
+
+/**
+ * multiply - multiplies two positive numbers
+ * @num1: first number as string
+ * @num2: second number as string
+ */
+void multiply(char *num1, char *num2)
+{
+	int len1, len2, len_res, i, j, carry, n1, n2, *res;
+
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len_res = len1 + len2;
+	res = malloc(sizeof(int) * len_res);
+	if (!res)
+		exit(98);
+
+	for (i = 0; i < len_res; i++)
+		res[i] = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		n1 = num1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			carry += res[i + j + 1] + (n1 * n2);
+			res[i + j + 1] = carry % 10;
+			carry /= 10;
+		}
+		res[i + j + 1] += carry;
+	}
+
+	print_result(res, len_res);
 	free(res);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+		errors();
+
+	multiply(argv[1], argv[2]);
 	return (0);
 }
