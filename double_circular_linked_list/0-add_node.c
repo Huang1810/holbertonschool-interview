3e672d70fbb7000f91562e06ca98a867d395072e
Huang1810 <ahhamed1810@gmail.com>
2025-10-04 22:05:16 +0100

    commit

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "list.h"
 
+
 /**
  * add_node_end - Adds a node to the end of a double circular linked list
  * @list: pointer to the head of the list
@@ -10,37 +11,38 @@
  */
 List *add_node_end(List **list, char *str)
 {
-    List *new_node, *tail;
+	List *new_node, *tail;
 
-    new_node = malloc(sizeof(List));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    if (!*list)
-    {
-        new_node->next = new_node;
-        new_node->prev = new_node;
-        *list = new_node;
-    }
-    else
-    {
-        tail = (*list)->prev;
+	if (!*list)
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		tail = (*list)->prev;
 
-        new_node->next = *list;
-        new_node->prev = tail;
-        tail->next = new_node;
-        (*list)->prev = new_node;
-    }
+		new_node->next = *list;
+		new_node->prev = tail;
+		tail->next = new_node;
+		(*list)->prev = new_node;
+	}
 
-    return (new_node);
+	return (new_node);
 }
+
 
 /**
  * add_node_begin - Adds a node to the beginning of a double circular list
@@ -50,8 +52,9 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-    List *new_node = add_node_end(list, str);
-    if (new_node)
-        *list = new_node;
-    return (new_node);
+	List *new_node = add_node_end(list, str);
+
+	if (new_node)
+		*list = new_node;
+	return (new_node);
 }
