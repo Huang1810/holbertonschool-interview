5627038bcd74a79b87031a07e5defffc3ab5bc97
Huang1810 <ahhamed1810@gmail.com>
2025-10-11 22:58:51 +0100

    commit

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -10,15 +10,15 @@
  */
 int get_max(int *array, size_t size)
 {
-    int max = array[0];
-    size_t i;
+	int max = array[0];
+	size_t i;
 
-    for (i = 1; i < size; i++)
-    {
-        if (array[i] > max)
-            max = array[i];
-    }
-    return (max);
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	return (max);
 }
 
 /**
@@ -29,33 +29,33 @@ int get_max(int *array, size_t size)
  */
 void counting_sort_radix(int *array, size_t size, int exp)
 {
-    int *output, count[10] = {0};
-    size_t i;
+	int *output, count[10] = {0};
+	size_t i;
 
-    output = malloc(size * sizeof(int));
-    if (!output)
-        return;
+	output = malloc(size * sizeof(int));
+	if (!output)
+		return;
 
-    /* Count occurrences of each digit */
-    for (i = 0; i < size; i++)
-        count[(array[i] / exp) % 10]++;
+	/* Count occurrences of each digit */
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
 
-    /* Change count[i] so that it contains actual position */
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
+	/* Change count[i] so that it contains actual position */
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
 
-    /* Build the output array (stable sort) */
-    for (i = size - 1; (int)i >= 0; i--)
-    {
-        output[count[(array[i] / exp) % 10] - 1] = array[i];
-        count[(array[i] / exp) % 10]--;
-    }
+	/* Build the output array (stable sort) */
+	for (i = size - 1; (int)i >= 0; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
 
-    /* Copy output array to original array */
-    for (i = 0; i < size; i++)
-        array[i] = output[i];
+	/* Copy output array to original array */
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-    free(output);
+	free(output);
 }
 
 /**
@@ -65,17 +65,17 @@ void counting_sort_radix(int *array, size_t size, int exp)
  */
 void radix_sort(int *array, size_t size)
 {
-    int max, exp;
+	int max, exp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    max = get_max(array, size);
+	max = get_max(array, size);
 
-    /* Sort for each digit, starting from least significant */
-    for (exp = 1; max / exp > 0; exp *= 10)
-    {
-        counting_sort_radix(array, size, exp);
-        print_array(array, size);
-    }
+	/* Sort for each digit, starting from least significant */
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		counting_sort_radix(array, size, exp);
+		print_array(array, size);
+	}
 }
